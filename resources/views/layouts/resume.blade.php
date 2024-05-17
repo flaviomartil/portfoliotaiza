62711cf09dc4bfb402604b16e2014a28e43dcb4b
Flavio Martil <flavio.martil@om30tech.com.br>
2024-05-17 16:50:49 -0300

    Resume a little smaller

--- resources/views/layouts/resume.blade.php
+++ resources/views/layouts/resume.blade.php
@@ -43,27 +43,49 @@
                     </p>
                     <h4>EDUCAÇÃO</h4>
                     <p>
-                        Graduação em Direito, Unip - 2/2021 a 12/2026 (cursando) <br>
+                        Graduação em Direito, Unip - 2/2021 a 12/2025 (cursando) <br>
                         Técnico em Administração, ETEC Philadelpho Gouvêa Netto - 8/2020 a 12/2021 (concluído) <br>
                         Ensino Médio - Completo em E.E Padre Clemente Marton Segura - 2/2006 a 12/2016 (concluído)
                     </p>
                 </div>
-                <div class="col-md-12 animated fadeInRight">
-                    <div class="subTitle"><h4>COMPETÊNCIAS <span>COMPORTAMENTAIS</span></h4></div>
-                    <ul>
-                        <li>Forte relacionamento interpessoal.</li>
-                        <li>Automotivado.</li>
-                        <li>Boa comunicação.</li>
-                        <li>Facilidade em trabalho em equipe.</li>
-                        <li>Adaptabilidade e resiliência.</li>
-                        <li>Vontade de crescimento.</li>
-                        <li>Bom humor.</li>
-                        <li>Criatividade e iniciativa.</li>
-                    </ul>
+                <div class="col-md-12 animated fadeInDown">
+                    <div class="row">
+                        <div class="subTitle"><h4>EXPERIÊNCIA</h4></div>
+                        <div class="col-md-6 animated fadeInRight">
+                            <h4>Estágio Direito | <span>Agu Advocacia Geral Da União</span></h4>
+                            <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>5/02/2024 até o momento</small></div>
+                            <ul>
+                                <li>Lançamentos e Consultas nos sistemas Institucionais</li>
+                                <li>Auxílio supervisionado na elaboração de peças e ofícios</li>
+                                <li>Visitas supervisionadas a Fóruns Federais/Estaduais</li>
+                                <li>Pesquisas doutrinárias e jurisprudenciais.</li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 animated fadeInRight">
+                            <h4>Estágio Direito | <span>Secretaria Municipal Da Fazenda</span></h4>
+                            <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>20/01/2023 até 20/01/2024</small></div>
+                            <ul>
+                                <li>Auxílio Na Leitura E Análise De Documentos</li>
+                                <li>Apoio Na Elaboração De Relatórios De Processos Administrativos</li>
+                                <li>Auxílio Nas Pesquisas De Jurisprudências, Doutrinas E Legislações</li>
+                                <li>Auxílio No Controle De Arquivos</li>
+                                <li>Auxílio Nos Protocolos De Documentos</li>
+                                <li>Apoio Na Abertura, Digitalização E Cópias De Processos Administrativos</li>
+                                <li>Apoio Na Alimentação E Atualização Dos Bancos De Dados</li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 animated fadeInRight">
+                            <h4>Estágio Direito |  <span>Consult Us Empresarial</span> </h4>
+                            <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>01/10/2021 até 30/12/2022</small></div>
+                            <ul>
+                                <li>Fornecer suporte geral às rotinas administrativas do escritório.</li>
+                                <li>Acompanhamento de notícias e legislação relacionadas a projetos.</li>
+                                <li>Auxiliar no atendimento a clientes.</li>
+                                <li>Auxiliar nas rotinas administrativas jurídicas.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
     <div id="courses_section">
         <div class="col-md-12 animated fadeInDown">
             <div class="pageTitle"><h4> CURSOS E  <span>ESPECIALIZAÇÕES</span></h4></div>
@@ -108,41 +130,20 @@
             </div>
         </div>
     </div>
-    <div class="col-md-12 animated fadeInDown">
-        <div class="row">
-            <div class="subTitle"><h4>EXPERIÊNCIA</h4></div>
-            <div class="col-md-6 animated fadeInRight">
-                <h4>Estágio Direito | <span>Agu Advocacia Geral Da União</span></h4>
-                <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>5/02/2024 até o momento</small></div>
-                <ul>
-                    <li>Lançamentos e Consultas nos sistemas Institucionais</li>
-                    <li>Auxílio supervisionado na elaboração de peças e ofícios</li>
-                    <li>Visitas supervisionadas a Fóruns Federais/Estaduais</li>
-                    <li>Pesquisas doutrinárias e jurisprudenciais.</li>
-                </ul>
-            </div>
-            <div class="col-md-6 animated fadeInRight">
-                <h4>Estágio Direito | <span>Secretaria Municipal Da Fazenda</span></h4>
-                <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>20/01/2023 até 20/01/2024</small></div>
-                <ul>
-                    <li>Auxílio Na Leitura E Análise De Documentos</li>
-                    <li>Apoio Na Elaboração De Relatórios De Processos Administrativos</li>
-                    <li>Auxílio Nas Pesquisas De Jurisprudências, Doutrinas E Legislações</li>
-                    <li>Auxílio No Controle De Arquivos</li>
-                    <li>Auxílio Nos Protocolos De Documentos</li>
-                    <li>Apoio Na Abertura, Digitalização E Cópias De Processos Administrativos</li>
-                    <li>Apoio Na Alimentação E Atualização Dos Bancos De Dados</li>
-                </ul>
-            </div>
-            <div class="col-md-6 animated fadeInRight">
-                <h4>Estágio Direito |  <span>Consult Us Empresarial</span> </h4>
-                <div class="momentDiv" style="margin-bottom: 20px;margin-top: -0.9%;"><small>01/10/2021 até 30/12/2022</small></div>
-                <ul>
-                    <li>Fornecer suporte geral às rotinas administrativas do escritório.</li>
-                    <li>Acompanhamento de notícias e legislação relacionadas a projetos.</li>
-                    <li>Auxiliar no atendimento a clientes.</li>
-                    <li>Auxiliar nas rotinas administrativas jurídicas.</li>
-                </ul>
+
+                <div class="col-md-12 animated fadeInRight">
+                    <div class="subTitle"><h4>COMPETÊNCIAS <span>COMPORTAMENTAIS</span></h4></div>
+                    <ul>
+                        <li>Forte relacionamento interpessoal.</li>
+                        <li>Automotivado.</li>
+                        <li>Boa comunicação.</li>
+                        <li>Facilidade em trabalho em equipe.</li>
+                        <li>Adaptabilidade e resiliência.</li>
+                        <li>Vontade de crescimento.</li>
+                        <li>Bom humor.</li>
+                        <li>Criatividade e iniciativa.</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
